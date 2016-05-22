5d50a8f8a6885c2a70d53aa5e90d2a5f7554cedf
anydasa <anydasa@gmail.com>
2016-05-22 22:18:04 +0300

    add example

---
+++ example/compass/config.rb
@@ -0,0 +1,13 @@
+require 'compass/import-once/activate'
+
+http_images_dir = "img"
+css_dir = "./../public/css"
+sass_dir = "./sass/"
+images_dir = "./img/"
+generated_images_dir = "./../public/img"
+
+#sourcemap = true
+cache = false
+output_style = :expanded
+relative_assets = false
+#line_comments = false
